19f8b4730c1526d8a8af64084577814608ccd456
wivest <120594169+wivest@users.noreply.github.com>
2024-04-10 22:33:33 +0200

    feat: add incubation time of Cocoon Gene

--- scenes/cocoon/cocoon.gd
+++ scenes/cocoon/cocoon.gd
@@ -2,14 +2,12 @@ class_name Cocoon extends Sprite2D
 
 signal incubated(cocoon_position: Vector2, genome: Genome)
 
-@export var incubation_time: float = 1 # time before turning into a creature
-
 var genome: Genome
 
 @onready var timer: Timer = $Timer
 
 func _ready():
-	timer.start(incubation_time)
+	timer.start(genome.incubation_time.value)
 
 func _on_timer_timeout():
 	incubated.emit(position, genome)
--- scenes/creature/scripts/genome.gd
+++ scenes/creature/scripts/genome.gd
@@ -2,7 +2,9 @@ class_name Genome extends Resource
 
 @export var speed: Gene
 @export var view_radius: Gene
+@export var incubation_time: Gene
 
 func mutate():
 	speed.mutate()
 	view_radius.mutate()
+	incubation_time.mutate()
