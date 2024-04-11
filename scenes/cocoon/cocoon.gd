7fa51bf686af83fac225b2993e3c0d8b2658c5d6
wivest <120594169+wivest@users.noreply.github.com>
2024-04-11 22:06:38 +0200

    feat: add color of Cocoon based on the time left

--- scenes/cocoon/cocoon.gd
+++ scenes/cocoon/cocoon.gd
@@ -2,12 +2,19 @@ class_name Cocoon extends Sprite2D
 
 signal incubated(cocoon_position: Vector2, genome: Genome)
 
+const INCUBATION_TIME_CAP: float = 2
+
+@export var incubation_color: Gradient
+
 var genome: Genome
 
 @onready var timer: Timer = $Timer
 
 func _ready():
 	timer.start(genome.incubation_time.value)
+
+func _process(_delta):
+	modulate = incubation_color.sample(timer.time_left / INCUBATION_TIME_CAP)
 
 func _on_timer_timeout():
 	incubated.emit(position, genome)
