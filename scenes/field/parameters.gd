839e839d13b69389556da6d31eb2a0848df5a1dc
wivest <120594169+wivest@users.noreply.github.com>
2024-04-18 22:12:43 +0200

    style: add export_group in SimulationParameters

--- scenes/field/parameters.gd
+++ scenes/field/parameters.gd
@@ -5,6 +5,7 @@ extends Resource
 @export var food_spawn_rate: float = 10
 @export var creatures_on_start: float = 10
 
+@export_group("Creature energy")
 @export var energy_on_start: float = 400 # subject to change
 @export var division_lower_bound: float = 800 # subject to change
 @export var energy_consumed_on_division: float = 400 # subject to change
