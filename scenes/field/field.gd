020b30a31145c72c2993083366a6afb0b3140f35
wivest <120594169+wivest@users.noreply.github.com>
2024-04-18 20:41:49 +0200

    feat: create SimulationParameters

--- scenes/field/field.gd
+++ scenes/field/field.gd
@@ -1,21 +1,12 @@
+class_name Field
 extends Node2D
 
-@export var simulation_speed: float = 1:
-	set = set_simulation_speed
-
-var _was_ready: bool = false
+@export var parameters: SimulationParameters
 
 @onready var creature_manager: CreatureManager = $CreatureManager
 @onready var spawner: Spawner = $Spawner
 
 func _ready():
-	_was_ready = true
-	set_simulation_speed(simulation_speed)
-
-func set_simulation_speed(value: float):
-	simulation_speed = value
-	if not _was_ready:
-		return
-	creature_manager.simulation_speed = value
-	creature_manager.simulation_speed_changed.emit(value)
-	spawner.simulation_speed = simulation_speed
+	creature_manager.simulation_speed = parameters.speed
+	creature_manager.simulation_speed_changed.emit(parameters.speed)
+	spawner.simulation_speed = parameters.speed
---
+++ scenes/field/parameters.gd
@@ -0,0 +1,6 @@
+class_name SimulationParameters
+extends Resource
+
+@export var speed: float = 1
+@export var food_spawn_rate: float = 10
+@export var creatures_on_start: float = 10
