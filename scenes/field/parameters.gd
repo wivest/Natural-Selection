634d08a0532151c672ef2a1a27303afbb925e035
wivest <120594169+wivest@users.noreply.github.com>
2024-04-20 13:46:49 +0200

    feat: create export_group Spawner

--- scenes/field/parameters.gd
+++ scenes/field/parameters.gd
@@ -5,11 +5,18 @@ extends Resource
 	set(value):
 		speed = value
 		changed.emit()
+
+@export_group("Spawner")
 @export var food_spawn_rate: float = 10:
 	set(value):
 		food_spawn_rate = value
 		changed.emit()
-@export var creatures_on_start: float = 10
+@export var food_on_start: int = 10 # one-time property
+@export var creatures_spawn_rate: float = 0:
+	set(value):
+		creatures_spawn_rate = value
+		changed.emit()
+@export var creatures_on_start: int = 10 # one-time property
 
 @export_group("Creature energy")
 @export var energy_on_start: float = 400 # subject to change
