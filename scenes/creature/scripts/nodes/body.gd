bda7adef6fbd2414899a443aad8921c784493030
wivest <120594169+wivest@users.noreply.github.com>
2024-04-21 19:06:51 +0200

    refactor: update color in separate function

--- scenes/creature/scripts/nodes/body.gd
+++ scenes/creature/scripts/nodes/body.gd
@@ -9,7 +9,11 @@ var _creature: Creature
 
 func _ready():
 	_creature = owner
+	update_color()
 
 func _process(_delta):
+	update_color()
+
+func update_color():
 	var speed := _creature.genome.speed.value
 	modulate = speed_color.sample(speed / SPEED_COLOR_CAP) # set color based on speed
