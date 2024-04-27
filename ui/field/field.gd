5a328368f4df8888301ec45bb9b052ff6a60f976
wivest <120594169+wivest@users.noreply.github.com>
2024-04-27 21:44:25 +0200

    feat: create UI field

---
+++ ui/field/field.gd
@@ -0,0 +1,25 @@
+@tool
+extends HBoxContainer
+
+signal field_changed(v: float)
+
+@export var field_name: String:
+	set(v):
+		field_name = v
+		if name_label != null:
+			name_label.text = field_name
+@export var start: float:
+	set(v):
+		start = v
+		if value_box != null:
+			value_box.value = start
+
+@onready var name_label: Label = $Name
+@onready var value_box: SpinBox = $Value
+
+func _ready():
+	name_label.text = field_name
+	value_box.value = start
+
+func _on_value_changed(v: float):
+	field_changed.emit(v)
