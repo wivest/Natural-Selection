e32789d2313ead281f321cebdc54f05d3a1034ba
wivest <120594169+wivest@users.noreply.github.com>
2024-04-27 22:07:42 +0200

    feat: add UI fields

--- ui/tab_container.gd
+++ ui/tab_container.gd
@@ -7,3 +7,9 @@ func _on_parameters_set(p: SimulationParameters):
 
 func _on_on_start_field_changed(v: float):
 	parameters.energy_on_start = v
+
+func _on_lower_bound_field_changed(v: float):
+	parameters.division_lower_bound = v
+
+func _on_on_division_field_changed(v: float):
+	parameters.energy_consumed_on_division = v
