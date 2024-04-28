a191ba0e836e5d5988c1491e18b080160cd321c2
wivest <120594169+wivest@users.noreply.github.com>
2024-04-28 22:12:26 +0200

    feat: connect signals from tab

--- ui/tab_container.gd
+++ ui/tab_container.gd
@@ -13,3 +13,9 @@ func _on_lower_bound_field_changed(v: float):
 
 func _on_on_division_field_changed(v: float):
 	parameters.energy_consumed_on_division = v
+
+func _on_spawner_on_start_field_changed(v: float):
+	parameters.food_on_start = v as int
+
+func _on_spawn_rate_field_changed(v: float):
+	parameters.food_spawn_rate = v
