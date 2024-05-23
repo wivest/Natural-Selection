882e7b76bd5627a3db5bfaaeccceb7e268f342b6
wivest <120594169+wivest@users.noreply.github.com>
2024-05-23 23:39:54 +0200

    feat: create Holder

---
+++ holder.gd
@@ -0,0 +1,13 @@
+class_name Holder
+extends Node
+
+@export var item: PackedScene
+
+func create(at_position: Vector2, angle: float=0) -> Node2D:
+	var instance: Node2D = item.instantiate()
+	instance.position = at_position
+	instance.rotation_degrees = angle
+	return instance
+
+func add(node: Node2D):
+	add_child(node)
