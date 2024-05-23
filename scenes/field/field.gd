038f1f72aed018e1f39eae3f81d5743cb86c287d
wivest <120594169+wivest@users.noreply.github.com>
2024-05-24 00:51:00 +0200

    feat: create new Creatures on signal

--- scenes/field/field.gd
+++ scenes/field/field.gd
@@ -7,16 +7,25 @@ extends Node2D
 
 func _ready():
 	for i in range(Parameters.data.creature_spawner.on_start):
-		var creature = creatures.create(area.get_random_point())
-		creatures.add(creature)
+		create_creature(area.get_random_point(), null)
 
 	for i in range(Parameters.data.food_spawner.on_start):
 		spawn_food()
 
+func create_creature(at_position: Vector2, genes: Genome):
+	var creature: Creature = creatures.create(at_position)
+	if genes != null:
+		creature.genome = genes
+	creature.born.connect(_on_creature_born)
+	creatures.add(creature)
+
 func spawn_food():
 	var angle: float = randf() * 360
-	var food = foods.create(area.get_random_point(), angle)
+	var food: Food = foods.create(area.get_random_point(), angle)
 	foods.add(food)
 
 func _on_spawner_timeout():
 	spawn_food()
+
+func _on_creature_born(at_position: Vector2, genes: Genome):
+	create_creature(at_position, genes)
