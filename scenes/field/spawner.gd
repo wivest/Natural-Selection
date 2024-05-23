596d4de8b732ed99bbf8948c5b6fd86a667236da
wivest <120594169+wivest@users.noreply.github.com>
2024-05-23 23:42:28 +0200

    feat: create Spawner as Timer node

---
+++ scenes/field/spawner.gd
@@ -0,0 +1,13 @@
+class_name Spawner
+extends Timer
+
+@export var rate: float = 0:
+	set(value):
+		rate = value
+		update_timer()
+
+func update_timer():
+	var wait_time_updated: float = 1 / (rate * Parameters.data.speed)
+	var correlation: float = wait_time_updated / wait_time
+	start(time_left * correlation)
+	wait_time = wait_time_updated
