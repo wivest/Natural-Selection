4c9b49cdde5ef6e7b3f0e240a512ec91d7df259d
wivest <120594169+wivest@users.noreply.github.com>
2024-06-04 23:39:48 +0200

    refactor: create Slider folder

---
+++ ui/slider/slider.gd
@@ -0,0 +1,19 @@
+extends HSlider
+
+@export var label_offset: float = 20
+
+@onready var label: Label = $Value
+
+func _ready():
+	label.offset_top = -label_offset
+	label.offset_bottom = -label_offset
+	update_label(value)
+
+func update_label(v: float):
+	label.text = str(v)
+	label.anchor_left = ratio
+	label.anchor_right = ratio
+
+func _on_value_changed(v: float):
+	Parameters.data.speed = v
+	update_label(v)
