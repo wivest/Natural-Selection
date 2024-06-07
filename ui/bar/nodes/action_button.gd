7c81ee1629ed56cff97f192679dea7c025a94626
wivest <120594169+wivest@users.noreply.github.com>
2024-06-07 22:38:14 +0200

    refactor: make event as property

--- ui/bar/nodes/action_button.gd
+++ ui/bar/nodes/action_button.gd
@@ -1,9 +1,10 @@
 extends Button
 
+var event: InputEventAction = shortcut.events[0]
+
 func _ready():
 	pressed.connect(_on_pressed)
 
 func _on_pressed():
-	var event: InputEventAction = shortcut.events[0]
 	Input.action_press(event.action)
 	Input.action_release(event.action)
