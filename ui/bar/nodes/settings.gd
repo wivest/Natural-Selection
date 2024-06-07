9f67b99d35213a7092611015e40cc8380a45f0ff
wivest <120594169+wivest@users.noreply.github.com>
2024-06-07 22:30:33 +0200

    reafctor: merge button scripts

--- ui/bar/nodes/settings.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	pressed.connect(_on_pressed)
-
-func _on_pressed():
-	var event: InputEventAction = shortcut.events[0]
-	Input.action_press(event.action)
-	Input.action_release(event.action)
