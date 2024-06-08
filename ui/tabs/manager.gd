89bf489a51537dcb8909e347725b5a65dc6b2da1
wivest <120594169+wivest@users.noreply.github.com>
2024-06-08 22:18:53 +0200

    feat: add statistics button

--- ui/tabs/manager.gd
+++ ui/tabs/manager.gd
@@ -1,17 +1,20 @@
 extends Control
 
-enum TabsState {NONE, SETTINGS}
+enum TabsState {NONE, SETTINGS, STATISTICS}
 
 var opened: TabsState = TabsState.NONE
 
 @onready var tabs: Dictionary = {
 	TabsState.NONE: $None,
-	TabsState.SETTINGS: $Settings
+	TabsState.SETTINGS: $Settings,
+	TabsState.STATISTICS: $Statistics
 }
 
 func _process(_delta):
 	if Input.is_action_just_pressed(&"open_settings"):
 		switch_state(TabsState.SETTINGS)
+	elif Input.is_action_just_pressed(&"open_statistics"):
+		switch_state(TabsState.STATISTICS)
 
 func switch_state(state: TabsState):
 	tabs[opened].visible = false
