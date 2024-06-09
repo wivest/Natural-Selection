096d87395b1787c2b7cc882d73279e62796b8559
wivest <120594169+wivest@users.noreply.github.com>
2024-06-09 23:41:33 +0200

    feat: create settings section

---
+++ ui/tabs/settings/section/parameter.gd
@@ -0,0 +1,2 @@
+class_name UIParameter
+extends HBoxContainer
---
+++ ui/tabs/settings/section/section.gd
@@ -0,0 +1,6 @@
+extends VBoxContainer
+
+@export var parameters: Array[UIParameter]
+
+func _on_parameter_changed(value: float, parameter_name: String):
+	pass
