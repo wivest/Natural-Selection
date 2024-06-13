a21fd6e4a6c86c006517f67c27808c02507e2c5e
wivest <120594169+wivest@users.noreply.github.com>
2024-06-13 23:51:13 +0200

    feat: add parameters from resource to node

--- parameters/parameters_node.gd
+++ parameters/parameters_node.gd
@@ -2,3 +2,24 @@ class_name ParametersNode
 extends Node
 
 var data: ParametersData = preload ("res://parameters/parameters.tres")
+
+signal speed_changed
+signal spawn_rate_changed
+
+@export var speed: float = 1:
+	set(value):
+		speed = value
+		speed_changed.emit()
+
+@export_group("Creature")
+@export var creatures: int = 10
+@export var energy_on_start: float = 400 # subject to change
+@export var division_lower_bound: float = 800 # subject to change
+@export var energy_consumed_on_division: float = 400 # subject to change
+
+@export_group("Food")
+@export var food: int = 10
+@export var spawn_rate: float = 10:
+	set(value):
+		spawn_rate = value
+		spawn_rate_changed.emit()
