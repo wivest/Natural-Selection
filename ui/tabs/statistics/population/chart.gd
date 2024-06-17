de52f4f1373b5bfb1ba79f724962be6cc0a21abf
wivest <120594169+wivest@users.noreply.github.com>
2024-06-17 23:53:09 +0200

    feat: make points time-dependent

--- ui/tabs/statistics/population/chart.gd
+++ ui/tabs/statistics/population/chart.gd
@@ -4,7 +4,7 @@ extends Control
 @export var step: float = 0.2
 @export var length_limit: int = 500
 
-var population: Array[int] = []
+var population: Array[Vector2] = []
 
 var _previous_step: float = 0
 
@@ -12,24 +12,27 @@ func _process(_delta):
 	var time := float(Time.get_ticks_msec()) / 1000
 	if time - _previous_step >= step / Parameters.speed:
 		_previous_step = time
-		population.append(Creature.count)
+		population.append(Vector2(time / Parameters.speed, Creature.count))
 		queue_redraw()
 
 	if Input.is_action_just_pressed(&"restart"):
 		population = []
 		_previous_step = time
-		population.append(Creature.count)
+		population.append(Vector2(time / Parameters.speed, Creature.count))
 
 func _draw():
-	var maximum: int = population.max()
+	var maximum: int = population.max().y
 	var prev: Vector2
 
-	var distance: float = size.x / min(population.size() - 1, length_limit)
 	var start_index: int = max(population.size() - length_limit, 0)
+	var start_time: float = population[start_index].x
+	var end_time: float = population[- 1].x
+	var delta: float = end_time - start_time
 
 	for i in range(start_index, population.size()):
-		var ratio: float = float(population[i]) / maximum
-		var pos := Vector2((i - start_index) * distance, size.y * (1 - ratio))
+		var ratio: float = population[i].y / maximum
+		var time_ratio: float = (population[i].x - start_time) / delta
+		var pos := Vector2(size.x * time_ratio, size.y * (1 - ratio))
 		if i != start_index:
 			draw_line(prev, pos, Color.GRAY)
 		draw_circle(pos, 3, Color.WHITE)
