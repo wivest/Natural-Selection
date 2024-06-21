62989343482cf04c77e10d24ff93e8dc6f8aac28
wivest <120594169+wivest@users.noreply.github.com>
2024-06-21 23:53:19 +0200

    feat: create delta counter

--- ui/tabs/statistics/population/counter.gd
+++ ui/tabs/statistics/population/counter.gd
@@ -1,4 +1,26 @@
 extends Label
 
+@export var step: float = 0.5
+
+var count: int
+
+var _count_delta: int
+var _count_previous: int
+var _time_previous: int
+
+func _ready():
+	count = Creature.count
+	_count_previous = count
+	_count_delta = 0
+	_time_previous = Time.get_ticks_msec()
+
 func _process(_delta):
-	text = str(Creature.count)
+	count = Creature.count
+
+	var time: int = Time.get_ticks_msec()
+	if time - _time_previous > step * 1000:
+		_count_delta = count - _count_previous
+		_count_previous = count
+		_time_previous = time
+
+	text = "Count: %d Delta: %d" % [count, _count_delta]
