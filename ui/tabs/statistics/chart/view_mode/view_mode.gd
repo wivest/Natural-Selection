880cca565f01e4f0308dcec53cdf7d00beed462b
wivest <120594169+wivest@users.noreply.github.com>
2024-06-27 00:12:31 +0200

    refactor: remove underscores in default implementation

--- ui/tabs/statistics/chart/view_mode/view_mode.gd
+++ ui/tabs/statistics/chart/view_mode/view_mode.gd
@@ -3,13 +3,13 @@ class_name ViewMode
 func get_start_index(_data: ChartData) -> int:
 	return 0
 
-func get_end_index(_data: ChartData) -> int:
-	return _data.nodes.size()
+func get_end_index(data: ChartData) -> int:
+	return data.nodes.size()
 
-func get_hratio(_value: float, _delta: float) -> float:
-	return _value / _delta
+func get_hratio(value: float, delta: float) -> float:
+	return value / delta
 
-func get_vratio(_value: float, _maximum: float) -> float:
-	if _maximum == 0:
+func get_vratio(value: float, maximum: float) -> float:
+	if maximum == 0:
 		return 0
-	return 1 - _value / _maximum
+	return 1 - value / maximum
