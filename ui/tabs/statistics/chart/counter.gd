cfe88af5e5283b172912db77b1f8ed3bdf4c65c7
wivest <120594169+wivest@users.noreply.github.com>
2024-06-28 23:31:57 +0200

    fix: apply getter to counter

--- ui/tabs/statistics/chart/counter.gd
+++ ui/tabs/statistics/chart/counter.gd
@@ -1,26 +1,27 @@
 extends Label
 
+@export var getter: Getter
 @export var step: float = 0.5
 
-var count: int
+var value: float
 
-var _count_delta: int
-var _count_previous: int
+var _value_delta: float
+var _value_previous: float
 var _time_previous: int
 
 func _ready():
-	count = Creature.count
-	_count_previous = count
-	_count_delta = 0
+	value = getter.get_value()
+	_value_previous = value
+	_value_delta = 0
 	_time_previous = Time.get_ticks_msec()
 
 func _process(_delta):
-	count = Creature.count
+	value = getter.get_value()
 
 	var time: int = Time.get_ticks_msec()
 	if time - _time_previous > step * 1000:
-		_count_delta = count - _count_previous
-		_count_previous = count
+		_value_delta = value - _value_previous
+		_value_previous = value
 		_time_previous = time
 
-	text = "Count: %d Delta: %d" % [count, _count_delta]
+	text = "Count: %d Delta: %d" % [value, _value_delta]
