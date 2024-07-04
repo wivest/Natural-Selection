f5e9b969caa184cbd0135d5277292034322404f4
wivest <120594169+wivest@users.noreply.github.com>
2024-07-04 02:00:21 +0200

    feat: create timer in parameters

--- ui/tabs/statistics/chart/parameters.gd
+++ ui/tabs/statistics/chart/parameters.gd
@@ -9,7 +9,11 @@ extends HBoxContainer
 @export var clear_button: Button
 
 @onready var counter: Counter = $Counter
+@onready var timer: TimerLow = $StepTimer
 
 func _ready():
 	counter.getter = getter
 	counter.padding = padding
+	timer.wait_time = step_spinbox.value
+
+	step_spinbox.value_changed.connect(func(v: float): timer.wait_time=v)
