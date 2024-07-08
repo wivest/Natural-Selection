78847ceb0e257e5690204ed358bd7b81deae64ab
wivest <120594169+wivest@users.noreply.github.com>
2024-07-08 02:40:47 +0200

    chore: remove debug _ready function

--- ui/tabs/statistics/chart/parameters/view_mode.gd
+++ ui/tabs/statistics/chart/parameters/view_mode.gd
@@ -1,10 +1,5 @@
 extends OptionButton
 
-func _ready():
-	var popup: PopupMenu = get_popup()
-	print(popup.position, popup.size)
-	popup.position = Vector2i(100, 100)
-
 func _on_pressed():
 	var popup: PopupMenu = get_popup()
 	popup.position.y = int(global_position.y) - popup.size.y
