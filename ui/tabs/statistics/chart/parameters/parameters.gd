b5b37efa12e0dc0c5b826c042724039b493be507
wivest <120594169+wivest@users.noreply.github.com>
2024-07-11 13:01:54 +0200

    refactor: remove counter from parameters

--- ui/tabs/statistics/chart/parameters/parameters.gd
+++ ui/tabs/statistics/chart/parameters/parameters.gd
@@ -8,11 +8,7 @@ extends HBoxContainer
 @export var clear: Button
 @export var timer: TimerLow
 
-@onready var counter: Counter = $Counter
-
 func _ready():
-	counter.getter = Getter.new() # TMP
-	counter.padding = padding
 	timer.wait_time = step.value
 
 	step.value_changed.connect(func(v: float): timer.wait_time=v)
