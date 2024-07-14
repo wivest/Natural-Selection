623c02c591be2340c4add174dfc19b551c0f5507
wivest <120594169+wivest@users.noreply.github.com>
2024-07-14 16:11:59 +0200

    feat: create genes dictionary

--- entities/creature/traits/genome.gd
+++ entities/creature/traits/genome.gd
@@ -5,10 +5,15 @@ extends Resource
 @export var view_radius: Gene
 @export var hatching_time: Gene
 
+@export var genes: Dictionary # [String, Gene] pairs
+
 func mutate():
 	speed.mutate()
 	view_radius.mutate()
 	hatching_time.mutate()
+
+	for gene: Gene in genes.values():
+		gene.mutate()
 
 func divide() -> Genome:
 	var g: Genome = duplicate(true)
@@ -20,17 +25,29 @@ func add(g: Genome):
 	view_radius.value += g.view_radius.value
 	hatching_time.value += g.hatching_time.value
 
+	for key: String in genes.keys():
+		genes[key].value += g[key].value
+
 func sub(g: Genome):
 	speed.value -= g.speed.value
 	view_radius.value -= g.view_radius.value
 	hatching_time.value -= g.hatching_time.value
+
+	for key: String in genes.keys():
+		genes[key].value -= g[key].value
 
 func div(n: int):
 	speed.value /= n
 	view_radius.value /= n
 	hatching_time.value /= n
 
+	for key: String in genes.keys():
+		genes[key].value /= n
+
 func zero():
 	speed.value = 0
 	view_radius.value = 0
 	hatching_time.value = 0
+
+	for key: String in genes.keys():
+		genes[key].value = 0
