768a7668877202712befaa2c0b883e19ddf8bca9
wivest <120594169+wivest@users.noreply.github.com>
2024-07-22 23:46:23 +0200

    feat: create factors category

--- parameters.gd
+++ parameters.gd
@@ -15,6 +15,10 @@ signal spawn_rate_changed
 @export var division_lower_bound: float = 800 # subject to change
 @export var energy_consumed_on_division: float = 400 # subject to change
 
+@export_group("Factors")
+@export var speed_multiplier: float = 1
+@export var speed_power: float = 1
+
 @export_group("Food")
 @export var food: int = 10
 @export var spawn_rate: float = 10:
