ce7000523528232965fc8c973c87914af42d246a
wivest <120594169+wivest@users.noreply.github.com>
2024-07-23 02:39:37 +0200

    refactor: delete section.gd script

--- ui/tabs/settings/sections/parameter.gd
+++ ui/tabs/settings/sections/parameter.gd
@@ -5,3 +5,10 @@ extends SpinBox
 
 func _init():
 	allow_greater = true
+
+func _ready():
+	value = Parameters.get(parameter_name)
+	value_changed.connect(_on_value_changed)
+
+func _on_value_changed(v: float):
+	Parameters.set(parameter_name, v)
