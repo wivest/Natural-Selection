1297c0c0a19ea4d3ca14298e9eae61f8189354c1
wivest <120594169+wivest@users.noreply.github.com>
2024-07-23 03:22:31 +0200

    refactor: update spawner when rate set

--- field/nodes/spawner.gd
+++ field/nodes/spawner.gd
@@ -1,15 +1,17 @@
 class_name Spawner
 extends TimerLow
 
-var rate: float = 0
+var rate: float = 0:
+	set(v):
+		rate = v
+		update_timer()
 
 func _ready():
-	update_timer()
-	Parameters.speed_changed.connect(update_timer)
-	Parameters.spawn_rate_changed.connect(update_timer)
+	rate = Parameters.spawn_rate
+	Parameters.speed_changed.connect(func(): rate=Parameters.spawn_rate)
+	Parameters.spawn_rate_changed.connect(func(): rate=Parameters.spawn_rate)
 
 func update_timer():
-	rate = Parameters.spawn_rate
 	var wait_time_updated: float = 1 / (rate * Parameters.speed)
 	if is_inside_tree():
 		var correlation: float = wait_time_updated / wait_time
