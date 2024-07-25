c7778836280d497e053f2ec15421a36731d1d864
wivest <120594169+wivest@users.noreply.github.com>
2024-07-25 22:44:17 +0200

    feat: create UI factor class

---
+++ ui/tabs/settings/sections/factor.gd
@@ -0,0 +1,15 @@
+class_name UIFactor
+extends SpinBox
+
+@export var gene_name: String
+@export var property_type: String
+
+func _init():
+	allow_greater = true
+
+func _ready():
+	value = Energy.factors[gene_name].get(property_type)
+	value_changed.connect(_on_value_changed)
+
+func _on_value_changed(v: float):
+	value = Energy.factors[gene_name].set(property_type, v)
