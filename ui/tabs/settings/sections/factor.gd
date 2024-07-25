1e8e316d0256bc4a034f692e730f454651a4d8c1
wivest <120594169+wivest@users.noreply.github.com>
2024-07-25 22:59:57 +0200

    feat: replace string with enum

--- ui/tabs/settings/sections/factor.gd
+++ ui/tabs/settings/sections/factor.gd
@@ -1,15 +1,29 @@
 class_name UIFactor
 extends SpinBox
 
+enum PropertyType {
+	MULTIPLIER,
+	POWER,
+}
+
 @export var gene_name: String
-@export var property_type: String
+@export var property_type: PropertyType
+
+var _property_name: String:
+	get:
+		match property_type:
+			PropertyType.MULTIPLIER:
+				return "multiplier"
+			PropertyType.POWER:
+				return "power"
+		return ""
 
 func _init():
 	allow_greater = true
 
 func _ready():
-	value = Energy.factors[gene_name].get(property_type)
+	value = Energy.factors[gene_name].get(_property_name)
 	value_changed.connect(_on_value_changed)
 
 func _on_value_changed(v: float):
-	Energy.factors[gene_name].set(property_type, v)
+	Energy.factors[gene_name].set(_property_name, v)
