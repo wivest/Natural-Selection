15400d433a6cd132d45e620a03748ca4f4beee10
wivest <120594169+wivest@users.noreply.github.com>
2024-07-27 13:17:46 +0200

    feat: add math formula in UI factor

--- ui/tabs/settings/sections/gene_factors.gd
+++ ui/tabs/settings/sections/gene_factors.gd
@@ -10,18 +10,21 @@ func add_factors():
 
 func create_factor(gene_name: String) -> HBoxContainer:
 	var container: HBoxContainer = HBoxContainer.new()
-	var label: Label = Label.new()
+	var factor_name: Label = Label.new()
+	var math_expr: Label = Label.new()
 	var factor_m: UIFactor = UIFactor.new()
 	var factor_p: UIFactor = UIFactor.new()
 
-	label.text = gene_name.capitalize()
+	factor_name.text = gene_name.capitalize()
+	math_expr.text = "* %d ^" % Creature.average.genes[gene_name].value
 	factor_m.gene_name = gene_name
 	factor_m.property_type = UIFactor.PropertyType.MULTIPLIER
 	factor_p.gene_name = gene_name
 	factor_p.property_type = UIFactor.PropertyType.POWER
 
-	container.add_child(label)
+	container.add_child(factor_name)
 	container.add_child(factor_m)
+	container.add_child(math_expr)
 	container.add_child(factor_p)
 
 	return container
