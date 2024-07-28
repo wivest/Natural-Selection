ebdbc08f744fca2939ec87aa958f9e255e9ab400
wivest <120594169+wivest@users.noreply.github.com>
2024-07-28 22:30:21 +0200

    feat: replace text with SpinBox

--- ui/tabs/settings/sections/gene_factors.gd
+++ ui/tabs/settings/sections/gene_factors.gd
@@ -11,21 +11,27 @@ func add_factors():
 func create_factor(gene_name: String) -> HBoxContainer:
 	var container: HBoxContainer = HBoxContainer.new()
 	var factor_name: Label = Label.new()
-	var math_expr: Label = Label.new()
+	var math_expr1: Label = Label.new()
+	var math_expr2: Label = Label.new()
 	var factor_m: UIFactor = UIFactor.new()
 	var factor_p: UIFactor = UIFactor.new()
+	var gene_value: SpinBox = SpinBox.new()
 
 	factor_name.text = gene_name.capitalize()
 	factor_name.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	math_expr.text = "* %d ^" % Creature.average.genes[gene_name].value
+	math_expr1.text = "*"
+	math_expr2.text = "^"
 	factor_m.gene_name = gene_name
 	factor_m.property_type = UIFactor.PropertyType.MULTIPLIER
 	factor_p.gene_name = gene_name
 	factor_p.property_type = UIFactor.PropertyType.POWER
+	gene_value.value = Creature.average.genes[gene_name].value
 
 	container.add_child(factor_name)
 	container.add_child(factor_m)
-	container.add_child(math_expr)
+	container.add_child(math_expr1)
+	container.add_child(gene_value)
+	container.add_child(math_expr2)
 	container.add_child(factor_p)
 
 	return container
