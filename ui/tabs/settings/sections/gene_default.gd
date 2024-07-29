44974945d5ad83ced23d54a5ab356b2f5e87db0b
wivest <120594169+wivest@users.noreply.github.com>
2024-07-29 22:20:21 +0200

    feat: add gene name and creature scene

--- ui/tabs/settings/sections/gene_default.gd
+++ ui/tabs/settings/sections/gene_default.gd
@@ -1,6 +1,9 @@
 class_name GeneDefault
 extends SpinBox
 
+@export var creature_scene: PackedScene
+@export var gene_name: String
+
 func _init():
 	allow_greater = true
 	step = 0.1
@@ -9,4 +12,6 @@ func _ready():
 	value_changed.connect(_on_value_changed)
 
 func _on_value_changed(v: float):
-	pass
+	var instance: Creature = creature_scene.instantiate()
+	instance.genome.genes[gene_name].value = v
+	# pack scene into file
