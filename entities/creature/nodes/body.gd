a00117093379e03bc923868bbb29a670649f46eb
wivest <120594169+wivest@users.noreply.github.com>
2024-08-11 20:22:01 +0200

    feat: create color speed minimum

--- entities/creature/nodes/body.gd
+++ entities/creature/nodes/body.gd
@@ -1,6 +1,7 @@
 class_name Body
 extends Sprite2D
 
+static var color_speed_min: float = 100
 static var speed_color_cap: float = 100:
 	set(value):
 		speed_color_cap = value
@@ -11,4 +12,4 @@ func _process(_delta):
 	update_color(owner.genome.genes["speed"].value)
 
 func update_color(speed: float):
-	modulate = speed_color.sample(speed / speed_color_cap) # set color based on speed
+	modulate = speed_color.sample((speed - color_speed_min) / speed_color_cap) # set color based on speed
